e91b2ba1ae5801c87396909efca86de6dd6b76f5
HectorMtz22 <hec.gre81@gmail.com>
2021-08-12 20:15:50 -0500

    Game Logic

--- game.js
+++ game.js
@@ -21,6 +21,64 @@ let apple = {
   spawned: false,
 };
 
-function update() {}
+const restart = () => {
+  score = 0;
+  isGameOver = false;
+  snake = {
+    size: 3,
+    direction: "d",
+    tail: [],
+  };
+  apple = {
+    x: 0,
+    y: 0,
+    spawned: false,
+  };
 
-setInterval(update, 1000 / 60);
+  for (let i = 0; i < 3; i++) {
+    snake.tail.push({ x: 0, y: 0 });
+  }
+};
+const draw = () => {
+  ctx.clearRect(0, 0, 700, 700);
+  ctx.fillStyle = "white";
+  for (let i = 0; i < snake.tail.length; i++) {
+    let point = snake.tail[i];
+    ctx.fillRect(point.x, point.y, 20, 20);
+  }
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(apple.x, apple.y, 20, 20);
+};
+
+const input = () => {};
+
+function update() {
+  for (let i = snake.tail.length - 1; i > 0; i--) {
+    let point = snake.tail[i];
+    let nextPoint = snake.tail[i - 1];
+
+    point.x = nextPoint.x;
+    point.y = nextPoint.y;
+
+    switch (snake.direction) {
+      case "d":
+        snake.tail[0].x += 20;
+        break;
+      case "w":
+        snake.tail[0].y -= 20;
+        break;
+      case "a":
+        snake.tail[0].x -= 20;
+        break;
+      case "s":
+        snake.tail[0].y += 20;
+        break;
+    }
+  }
+
+  draw();
+}
+
+restart();
+setInterval(update, 1000 / 10);
